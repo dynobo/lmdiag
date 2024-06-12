6641df4f877e35594229c5e6329c64bba24fc817
dynobo <dynobo@mailbox.org>
2024-06-12 02:25:04 +0200

    feat: add benchmarking script

---
+++ benchmark.py
@@ -0,0 +1,44 @@
+import timeit
+
+import numpy as np
+import statsmodels as sm
+import statsmodels.api as sma
+
+import lmdiag
+
+df = sma.datasets.get_rdataset("ames", "openintro").data
+lm = sm.formula.api.ols("np.log10(price) ~ Q('Overall.Qual') + np.log(area)", df).fit()
+
+lm_stats = lmdiag.plots.init_model_stats(lm)
+
+
+if __name__ == "__main__":
+    import timeit
+
+    for stmt in [
+        "lm_stats.residuals",
+        "lm_stats.fitted_values",
+        "lm_stats.standard_residuals",
+        "lm_stats.cooks_d",
+        "lm_stats.leverage",
+        "lm_stats.params_count",
+        "lm_stats.sqrt_abs_residuals",
+        "lm_stats.normalized_quantiles",
+        "lmdiag.plot(lm)",
+        "lmdiag.resid_fit(lm)",
+        "lmdiag.q_q(lm)",
+        "lmdiag.scale_loc(lm)",
+        "lmdiag.resid_lev(lm)",
+    ]:
+        timing = timeit.repeat(
+            stmt=stmt,
+            globals=globals(),
+            number=1,
+            repeat=3,
+        )
+        print(  # noqa: T201
+            f"{stmt:<30} "
+            f"Max: {np.max(timing):.3f}"
+            f"\tMin: {np.min(timing):.3f}"
+            f"\tAvg: {np.mean(timing):.3f}"
+        )
